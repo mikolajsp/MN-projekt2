e31fca26eebaf56be9fbba72149262b0ff751cb0
Mikołaj Spytek <mikolajspytek@gmail.com>
2020-12-30 10:31:42 +0100

    implemented gaussian elimination with partial pivoting

---
+++ gepp.m
@@ -0,0 +1,38 @@
+function [Ar] = gepp(A,b)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+n = size(A, 1);
+
+if size(A,1) ~= size(A,2)
+    error("Macierz nie jest kwadratowa");
+end
+
+if size(b, 1) ~= n
+    error("wektor wyrazów wolnych ma nieprawidłowy rozmiar");
+end
+
+Ar = [A, b];
+
+for i = 1:n
+    %znajdowanie maksymalnego elementu w wierszu 
+    [maksimum, indeks] = max(abs(Ar([i n],i)));
+    indeks = indeks+i-1;
+    
+    %zamiana wierszy
+    Ar([i indeks],:) = Ar([indeks i],:);
+    
+    %wyzerowanie wierszy pod przekatna
+    
+    for j = (i+1):n
+       Ar(j,:) = Ar(j,:) - Ar(j, i)/Ar(i, i) * Ar(i, :);
+    end
+    
+    
+end
+
+
+
+
+end
+
